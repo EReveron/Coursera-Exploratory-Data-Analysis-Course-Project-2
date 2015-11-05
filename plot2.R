b8380258c905da6a133e037fe33fd3e35656dc18
EReveron <enrique.reveron@gmail.com>
2015-11-05 12:48:46 -0430

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,8 @@
 plot2 <- function() {   
 	## plot2.R
-	
-
-
+	## Create Plot2 requested by the Coursera Exploratory Data Analysis Course Project 2
 	## Written by: Enrique Reveron
-
-	## Set system locale to English to have the same output requested (strptime)
-	Sys.setlocale("LC_TIME", "English")
-
+	
 	## Check if the data file is located in the working dir
 
 	if (!file.exists("summarySCC_PM25.rds"))
@@ -29,13 +24,10 @@ plot2 <- function() {
 	## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") 
 	## from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
-
+	## Select the rows related with Maryland (fips==24510) and Aggregate (sum) the emissions by year
 	dt <- NEI[which(NEI$fips=="24510"),]
 
 	dt <- aggregate(Emissions ~ year, dt, sum)
-	
-
-
 	
 	## Create a png file, a Histogram and Plot it
 
@@ -47,8 +39,9 @@ plot2 <- function() {
     		res=72)
 
 	barplot(height=dt$Emissions, names.arg=dt$year, xlab="years", 
-		ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at Baltimore in various years'))
-
+		ylab="total PM2.5 emissions (tons)",
+		main="Total PM2.5 Emissions (1999-2008) at Baltimore City, Maryland", col = "blue")
+		
 	dev.off()
 
 }
